c57ba2f6ea88ed8ceebce521f2abd2790fe22ad9
Bowen <bowenbhwu@gmail.com>
2021-03-19 01:27:18 +0800

    Updated docs for validation.py

--- utils/validation.py
+++ utils/validation.py
@@ -1,35 +1,122 @@
+# -*- coding: utf-8 -*-
+"""The module provides functions related to input validation."""
+
 import re
 from validate_email import validate_email
 
+
 def verify_regex(regex: str, d: str) -> bool:
+    """Verifies data against a regular expression.
+
+    Note:
+        The provided regular expression should be a
+        uncompiled regular expression.
+
+    Args:
+        regex (str): The regular expression to be compiled.
+        d (str): A string object containing the data to be
+            verified.
+
+    Returns:
+        bool: `True` if matches. Otherwise, `False`.
+
+    """
     d = str(d)
     regex = re.compile(regex)
-    if regex.match(d) is None:
-        return False
-    return True
+    return False if regex.match(d) is None else True
+
 
 def is_money(d: str) -> bool:
+    """Checks if the provided data is in currency format.
+
+    Args:
+        d (str): The string to be verified.
+
+    Returns:
+        bool: `True` if matches. Otherwise, `False`.
+
+    """
     return verify_regex('^[0-9]+(\.[0-9]{1,2})?$', d)
 
-def is_rating(d):
-    # A rating should be an integer between 1 and 5
+def is_rating(d: str):
+    """Checks if the provided data is a valid rating.
+
+    The function returns `True` if the provided data
+    is between 1 and 5.
+
+    Args:
+        d (str): The string to be verified.
+
+    Returns:
+        bool: `True` if matches. Otherwise, `False`.
+
+    """
     return verify_regex('^[1-5]$', d)
 
+
 def is_valid_email(d: str):
+    """Checks if the provided email address is valid.
+
+    Note:
+        This function uses the packet `validate_email`.
+        It will communicate with the actual mail server
+        to verify the email address. Not just validating
+        against a regular expression. For more information,
+        please visit:
+        https://pypi.org/project/validate_email/
+
+    Args:
+        d (str): An email address to be verified.
+
+    Returns:
+        bool: `True` if the email is valid. Otherwise, 
+        `False` will be returned.
+
+    """
     return validate_email(d)
 
+
 def is_valid_length(d: str, min: int, max: int) -> bool:
-    # The function checks whether d's length is in the range [min, max]
+    """checks whether d's length is in the range [min, max].
+
+    Args:
+        d (str): The string to be verified.
+        min (int): The minimum expected length.
+        max (int): The maximum expected length.
+
+    Returns:
+        bool: `True` if d is within the range of [min, max]. 
+            Otherwise, `False`.
+
+    """
     d = str(d)
-    if (len(d) >= min and len(d) <= max):
-        return True
-    return False
+    return True if (len(d) >= min and len(d) <= max) else False
 
 def is_valid_username(
         d: str, min_len: int = 6, max_len: int = 24, 
         allow_lower: bool = True, allow_upper: bool = True,
         allow_digit: bool = True, allow_underscore: bool = True,
         allow_dash: bool = True) -> bool:
+    """Checks if the provided data is a valid username.
+
+    Note:
+        You may consider changing the default parameters 
+        accordingly to your policy.
+
+    Args:
+        d (str): The string to be verified.
+        min_len (int): The minimum length allowed.
+        max_len (int): The maximum length allowed.
+        allow_lower (bool): Allow for lower case letter?
+        allow_upper (bool): Allow for upper case letter?
+        allow_digit (bool): Allow for digits?
+        allow_underscore (bool): Allow for underscore (_)?
+        allow_dash (bool): Allow for dash (_)?
+
+    Returns:
+        bool: `True` if it is valid. Otherwise, `False`.
+
+    """
     return verify_regex(f"^[{'a-z' if allow_lower else ''}" + 
                         f"{'A-Z' if allow_upper else ''}" + 
                         f"{'0-9' if allow_digit else ''}" +
@@ -37,11 +124,35 @@ def is_valid_username(
                         f"{'-' if allow_dash else ''}" + 
                         f"]{{{min_len},{max_len}}}$", d)
 
+
 def is_valid_password(
         d: str, min_len: int = 8, max_len: int = 24, 
-        require_upper: bool = True, require_lower: bool = True, 
+        require_lower: bool = True, 
+        require_upper: bool = True, 
         require_digit: bool = True, 
-        require_special_char: bool = False) -> bool:    
+        require_special_char: bool = False) -> bool:
+    """Checks if the provided data is a valid password.
+
+    Note:
+        You may consider changing the default parameters 
+        accordingly to your policy.
+
+    Args:
+        d (str): The string to be verified.
+        min_len (int): The minimum length required.
+        max_len (int): The maximum length required.
+        require_lower (bool): Require at least one lower 
+            case letter?
+        require_upper (bool): Require at least one upper 
+            case letter?
+        require_digit (bool): Require at least one digits?
+        require_special_char (bool): Require special at 
+            least one characters (#?!@$ %^&*-)?
+
+    Returns:
+        bool: `True` if it is valid. Otherwise, `False`.
+
+    """
     assert min_len < max_len
     return verify_regex(f"^{'(?=.*[a-z])' if require_lower else '' }" + 
                         f"{'(?=.*[A-Z])' if require_upper else ''}" +
